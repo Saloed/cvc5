04da7af0b932775e023e89484dd6d40efb5e4152
Valentyn Sobol <napster_1997@mail.ru>
2024-04-12 12:09:39 +0300

    Build script

--- build_windows.sh
+++ build_windows.sh
@@ -1,6 +1,6 @@
 
-# Use MSYS2 MINGW64, Python must be installed on host windows system
-export PATH=$PATH:/c/Python312:/c/Python312/Scripts
+# Use MSYS2 MINGW64, Python must be installed on the host windows system
+export PATH="$PATH:/c/Python312/:/c/Python312/Scripts/"
 
 pacman -S \
   make \
@@ -9,10 +9,14 @@ pacman -S \
   mingw-w64-x86_64-gcc \
   mingw-w64-x86_64-gmp
 
-python3 -m pip install -r contrib/requirements_build.txt
+python -m pip install -r contrib/requirements_build.txt
 
-./configure.sh production --auto-download --ipo --no-static --no-cln --no-glpk --no-editline --no-poly --win64-native --prefix=$(pwd)/win-dist -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache --name=build-shared
+./configure.sh production --auto-download --ipo --no-static --no-cln --no-glpk --no-editline --no-poly \
+ --win64-native --prefix=$(pwd)/win-dist --name=build-shared \
+ -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
+ -DCMAKE_C_COMPILER_LAUNCHER=ccache
 
 cd build-shared/ && pwd=$(pwd)
 ccache --set-config="base_dir=$pwd"
 make -j4
+make install
